835eda99795219c87dffa6805b4d216176e6685e
rac0001 <rakesh.anantoju@gmail.com>
2020-12-24 12:12:53 -0600

    initial commit

---
+++ src/main/java/tech/coder/io/bgpostsapp/BgPostsAppApplication.java
@@ -0,0 +1,13 @@
+package tech.coder.io.bgpostsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgPostsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgPostsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/coder/io/bgpostsapp/BgPostsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.coder.io.bgpostsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgPostsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
